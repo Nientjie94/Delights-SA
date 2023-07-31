760c47c81666fcdfdb6e3df9d4ef740ea5acbad5
Merc5Dark <31131794siya@gmail.com>
2023-07-31 13:57:07 +0200

    fixed text

--- src/pages/recipes/index.js
+++ src/pages/recipes/index.js
@@ -9,35 +9,107 @@ export const recipesList = [
         title: 'Milktart',
         image: 'https://i.pinimg.com/564x/6e/38/54/6e3854dc81b843ee5d7bf0141d5c10e2.jpg',
         description: 'A South African dessert consisting of a creamy custard filling flavored with cinnamon, poured into a pastry crust, and dusted with cinnamon on top.,',
-        ingredients:['ingred1', 'ingred2', 'ingred3', 'ingred4', 'ingred5', ],
-        instructions: ['step one','step two']
+        ingredients: [
+            '1 cup milk',
+            '1/2 cup cream',
+            '1/4 cup condensed milk',
+            '1/4 cup Amarula cream liqueur (optional)',
+            '1/2 teaspoon vanilla extract',
+            'Ground cinnamon, for garnish', 
+        ],
+        instructions: [
+            'In a saucepan, heat the milk and cream over medium heat until it starts to simmer.',
+            'Add the condensed milk and stir until well combined.',
+            'Remove from the heat and let it cool to room temperature.',
+            'Stir in the Amarula cream liqueur (if using) and vanilla extract.',
+            'Pour the mixture into shot glasses and refrigerate until chilled.',
+            'Before serving, garnish with ground cinnamon for an elegant touch.',      
+        ]
     },
     {
         id: 'koeksisters-recipe',
         title: 'Koeksusters',
         image: 'https://i.pinimg.com/564x/a8/84/53/a884530c10a6ccf66ce315c7e44d5227.jpg',
-        ingredients:['ingred1', 'ingred2', 'ingred3', 'ingred4', 'ingred5', ],
         description: 'A traditional South African sweet treat made from fried dough, soaked in syrup, and braided into a twisted shape, offering a sticky and delightful indulgence.',
-        ingredients:['ingred1', 'ingred2', 'ingred3', 'ingred4', 'ingred5', ],
-        instructions: ['step one','step two']
+        ingredients: [
+            '4 cups all-purpose flour',
+            '1/2 cup milk',
+            '1/2 cup water',
+            '2 tablespoons butter',
+            '2 tablespoons sugar',
+            '1 packet (7g) instant yeast',
+            'Pinch of salt',
+            'Oil, for frying',
+       
+        ],
+        instructions: [
+            'Mix the crushed biscuits and melted butter in a bowl until well combined.',
+            'Press the biscuit mixture into the base of a greased springform pan to form the crust. Chill in the refrigerator for about 15 minutes.',
+            'In a separate bowl, beat the cream cheese and powdered sugar until smooth.',
+            'Gradually add the Amarula cream liqueur to the cream cheese mixture, mixing well.',
+            'Fold in the whipped cream until everything is evenly combined.',
+            'Pour the filling over the chilled crust and smooth the top with a spatula.',
+            'Refrigerate the cheesecake for at least 4 hours or until set.',
+            'Serve chilled, and you can garnish with chocolate shavings or fresh fruit if desired.',      
+        ]
     },
     {
         id: 'Peppermint-Crisp-recipe',
         title: 'Peppermint Crisp',
         image: 'https://i.pinimg.com/564x/a4/b2/71/a4b2718876dae62229bca36f608f7b7b.jpg',
-        ingredients:['ingred1', 'ingred2', 'ingred3', 'ingred4', 'ingred5', ],
         description: 'A classic South African dessert featuring layers of Tennis biscuits, whipped cream, and caramelized condensed milk, topped with peppermint chocolate for a refreshing and satisfying treat.',
-        ingredients:['ingred1', 'ingred2', 'ingred3', 'ingred4', 'ingred5', ],
-        instructions: ['step one','step two']
+        ingredients: [
+            '1 1/2 cups crushed digestive biscuits or graham crackers',
+            '1/4 cup melted butter',
+            '2 cups cream cheese',
+            '1 cup powdered sugar',
+            '1 cup Amarula cream liqueur',
+            '1 cup heavy cream, whipped to stiff peaks', 
+        ],
+        instructions: [
+            'Mix the crushed biscuits and melted butter in a bowl until well combined.',
+            'Press the biscuit mixture into the base of a greased springform pan to form the crust. Chill in the refrigerator for about 15 minutes.',
+            'In a separate bowl, beat the cream cheese and powdered sugar until smooth.',
+            'Gradually add the Amarula cream liqueur to the cream cheese mixture, mixing well.',
+            'Fold in the whipped cream until everything is evenly combined.',
+            'Pour the filling over the chilled crust and smooth the top with a spatula.',
+            'Refrigerate the cheesecake for at least 4 hours or until set.',
+            'Serve chilled, and you can garnish with chocolate shavings or fresh fruit if desired.',      
+        ]
     },
     {
         id: 'malva-pudding-recipe',
         title: 'Malva Pudding',
         image: 'https://i.pinimg.com/564x/ab/70/69/ab7069454ecb91b4e90595babc94ce67.jpg',
-        ingredients:['ingred1', 'ingred2', 'ingred3', 'ingred4', 'ingred5', ],
         description: 'A rich and moist South African dessert made with apricot jam, served warm and drenched in a sticky toffee sauce, providing a comforting and decadent culinary experience.',
-        ingredients:['ingred1', 'ingred2', 'ingred3', 'ingred4', 'ingred5', ],
-        instructions: ['step one','step two']
+        ingredients: [
+            '1 cup all-purpose flou',
+            '1 cup granulated sugar',
+            '1 tablespoon apricot jam',
+            '1 teaspoon baking soda',
+            '1 large egg',
+            '1 tablespoon butter, melted',
+            '1 cup milk',
+            '1 tablespoon vinegar',
+            'Pinch of salt',
+            'For the Sauce:',
+
+            '1 cup heavy cream',
+            '1 cup granulated sugar',
+            '1/2 cup butter',
+            '1/2 cup hot water', 
+        ],
+        instructions: [
+            'Preheat the oven to 350°F (175°C) and grease a baking dish.',
+            'In a large mixing bowl, mix the flour, sugar, and salt.',
+            'In a separate bowl, whisk the egg, melted butter, apricot jam, milk, and vinegar.',
+            'Combine the wet and dry ingredients and mix well.',
+            'Dissolve the baking soda in a little hot water and add it to the mixture.',
+            'Pour the batter into the greased baking dish and bake for about 30-35 minutes or until the pudding is golden and cooked through.',
+            'While the pudding is baking, prepare the sauce by combining all the sauce ingredients in a saucepan and heating until the sugar is dissolved.',
+            'As soon as the pudding is removed from the oven, pour the hot sauce over it, allowing it to soak in.',
+            'Serve the warm Malva Pudding with custard or vanilla ice cream.,'
+        ]
     },
 
 ]
